9ff32a6d5ef275b7d3dccaca7c2f41f14c08e3c6
Atnhai <atnhai@gmail.com>
2022-07-21 11:38:49 +0700

    add coin table

--- client/src/pages/CoinsPage.js
+++ client/src/pages/CoinsPage.js
@@ -8,6 +8,7 @@ import { getErrorMsg } from '../utils/helperFuncs';
 import { Typography, Chip, TextField, InputAdornment } from '@material-ui/core';
 import { useTagsPageStyles } from '../styles/muiStyles';
 import SearchIcon from '@material-ui/icons/Search';
+import './coin.css';
 
 const CoinsPage = () => {
   const { notify } = useStateContext();
@@ -20,6 +21,12 @@ const CoinsPage = () => {
   const [filterInput, setFilterInput] = useState('');
   const classes = useTagsPageStyles();
 
+  const data2 = [
+    { name: "Anom", age: 19, gender: "Male" },
+    { name: "Megha", age: 19, gender: "Female" },
+    { name: "Subham", age: 25, gender: "Male"},
+  ]
+
   return (
     <div className={classes.root}>
       <Typography variant="h5" className={classes.titleText} color="secondary">
@@ -30,7 +37,24 @@ const CoinsPage = () => {
         Good performances means asking and answering useful questions. The coins are determined by how many people upvote on your post. This is a chart of how coins
         are calculated and how much you can convert into money.
       </Typography>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        {data2.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+            </tr>
+          )
+        })}
+      </table>
     </div>
+    
   );
 };
 
