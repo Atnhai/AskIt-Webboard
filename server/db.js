c7c6481bff35c2618bba6613bb55b5645e3d2b0f
Atnhai <atnhai@gmail.com>
2022-07-27 11:23:13 +0700

    make it better

--- server/db.js
+++ server/db.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 const { MONGODB_URI: url } = require('./utils/config');
 
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const connectToDB = require('./db');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { PORT } = require('./utils/config');
+require('dotenv').config();
 connectToDB();
 
 const server = new ApolloServer({
@@ -11,6 +12,10 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req }),
 });
 
-server.listen({ port: PORT }).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+// server.listen({ port: PORT }).then(({ url }) => {
+//  console.log(`Server ready at ${url}`);
+// });
+
+server.listen(process.env.PORT || 3000 , () => {
+  console.log("You are connected!");
 });
